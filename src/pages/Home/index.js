d6819891deb230b2e312733d6f6e856c5a12fba7
Diego Rodrigues <diego.rodrigues3007@gmail.com>
2023-01-21 08:59:20 -0300

    login whatsapp

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import Button from "../../components/Button";
+// import useAuth from "../../hooks/useAuth";
+import * as C from "./styles";
+
+const Home = () => {
+  // const { signout } = useAuth();
+
+  return <C.Container></C.Container>;
+};
+
+export default Home;
---
+++ src/pages/Signin/styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+  height: 100vh;
+  background:#173540;
+`;
+
+export const Content = styled.div`
+  gap: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  box-shadow: 0 1px 2px #0003;
+  background-color: white;
+  max-width: 350px;
+  padding: 20px;
+  border-radius: 5px;
+  background:#3D3D3D;
+`;
+
+export const Label = styled.label`
+  font-size: 18px;
+  font-weight: 600;
+  color: #fff;
+`;
+
+export const LabelSignup = styled.label`
+  font-size: 16px;
+  color: #fff;
+`;
+
+export const labelError = styled.label`
+  font-size: 14px;
+  color: red;
+`;
+
+export const Strong = styled.strong`
+  cursor: pointer;
+
+  a {
+    text-decoration: none;
+    color: #217373;
+  }
+`;
